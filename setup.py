b915afc2b3c0af027f4804fcfccde8380174cce2
Harry Prudden <Harry@Harrys-MBP-72.lan>
2019-10-29 18:02:23 +0000

    Setup file added and source directory created

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 17:58:32 2019
+
+@author: Harry
+"""
+
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'flask-sqlalchemy',
+    'psycopg2',
+]
+
+setup(
+    name='FlaskDBWork',
+    version='0.0',
+    description='A To-Do List built with Flask',
+    author='Harry Prudden',
+    author_email='htprudden@gmail.com',
+    keywords='web flask',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires
+)
